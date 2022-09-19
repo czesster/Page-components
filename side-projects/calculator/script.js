957d69bce3e9591b7d709cf75f30cef714db9190
czesster <bloch.jakub@gmail.com>
2022-09-19 15:20:44 +0200

    used reduce on all operations, add . logic

--- side-projects/calculator/script.js
+++ side-projects/calculator/script.js
@@ -1,19 +1,7 @@
 "use strict";
 
-// const btn0 = document.querySelector(".btn--0");
-// const btn1 = document.querySelector(".btn--1");
-// const btn2 = document.querySelector(".btn--2");
-// const btn4 = document.querySelector(".btn--3");
-// const btn3 = document.querySelector(".btn--4");
-// const btn5 = document.querySelector(".btn--5");
-// const btn6 = document.querySelector(".btn--6");
-// const btn7 = document.querySelector(".btn--7");
-// const btn8 = document.querySelector(".btn--8");
-// const btn9 = document.querySelector(".btn--9");
-
 const btns = document.querySelectorAll(".btn--number");
 
-const btnDot = document.querySelector(".btn--dot");
 const btnResult = document.querySelector(".btn--res");
 const btnPlus = document.querySelector(".btn--pl");
 const btnMinus = document.querySelector(".btn--min");
@@ -26,16 +14,6 @@ const output = document.querySelector(".output-text");
 
 let outputArray = [];
 let action;
-
-// const displayOutput = function (arr) {
-//   outputArray.forEach((el) => {
-//     if (output.innerHTML === "0") {
-//       output.innerHTML = el;
-//     } else {
-//       output.innerHTML += el;
-//     }
-//   });
-// };
 
 const add = function () {
   outputArray.push(output.innerHTML);
@@ -85,43 +63,25 @@ const displayResult = function () {
   console.log(outputArray);
 
   //   Depend on what action have been selected during choosing numbers
-
   // ADD
   if (action === "+") {
-    // let sum = outputArray[0] * 1;
-    // for (let i = 1; i < outputArray.length; i++) {
-    //   sum += outputArray[i] * 1;
-    //   console.log(sum);
-    // }
     output.innerHTML = outputArray.reduce((acc, cur) => acc * 1 + cur * 1);
-    // output.innerHTML = sum;
 
     // DIVIDE
   } else if (action === "/") {
-    let div = outputArray[0] * 1;
-    console.log(div);
-    for (let i = 1; i < outputArray.length; i++) {
-      div /= outputArray[i] * 1;
-    }
-    output.innerHTML = div.toFixed(2);
+    // + in the beggining explaination:
+    // https://stackoverflow.com/a/12830454
+    output.innerHTML = +outputArray
+      .reduce((acc, cur) => ((acc * 1) / cur) * 1)
+      .toFixed(2);
 
     // SUBSTRACT
   } else if (action === "-") {
-    let sub = outputArray[0] * 1;
-    console.log(sub);
-    for (let i = 1; i < outputArray.length; i++) {
-      sub -= outputArray[i] * 1;
-    }
-    output.innerHTML = sub;
+    output.innerHTML = outputArray.reduce((acc, cur) => acc * 1 - cur * 1);
 
     // MULTIPLY
   } else if (action === "x") {
-    let mul = outputArray[0] * 1;
-    console.log(mul);
-    for (let i = 1; i < outputArray.length; i++) {
-      mul *= outputArray[i] * 1;
-    }
-    output.innerHTML = mul;
+    output.innerHTML = outputArray.reduce((acc, cur) => acc * 1 * cur * 1);
   }
 
   //   clear array for new operations
@@ -133,11 +93,9 @@ const displayResult = function () {
 // EVENT LISTENERS
 btns.forEach((btn) =>
   btn.addEventListener("click", function () {
-    if (output.innerHTML === "0") {
-      output.innerHTML = btn.innerHTML;
-    } else {
-      output.innerHTML += btn.innerHTML;
-    }
+    output.innerHTML === "0"
+      ? (output.innerHTML = btn.innerHTML)
+      : (output.innerHTML += btn.innerHTML);
     console.log(outputArray);
   })
 );
